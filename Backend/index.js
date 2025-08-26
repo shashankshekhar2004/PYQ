954ed20268b7ada360d9fda005bdee97a7f6194c
shashankshekhar2004 <shashank2002shekhar@gmail.com>
2025-08-27 03:51:27 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -20,9 +20,7 @@ const port = process.env.PORT || 3000;
 
 
 // Middleware setup
-app.use(cors(
-    { origin: ["https://princepyq.netlify.app","https://ankitpyq.netlify.app","https://mohitpyq.netlify.app", "http://localhost:3000", "http://localhost:5173", "http://localhost:3001", "http://localhost:5173/admin"] }
-)); // Enable Cross-Origin Resource Sharing
+app.use(cors()) // Enable Cross-Origin Resource Sharing
 app.use(express.json()); // Parse incoming JSON requests
 
 // connectToDatabase;
